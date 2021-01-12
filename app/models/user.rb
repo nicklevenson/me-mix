b408ba7caf70a78e56d3bacc37eb1ac5818917ac
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-12 15:42:43 -0800

    add to

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,7 @@ class User < ApplicationRecord
   has_many :mixes
   has_many :comments
   has_many :likes
-  has_many :mixes, through: :likes
-  has_many :mixes, through: :comments
-  
+ 
   validates :email, presence: true, uniqueness: true
   validates :password, length: {minimum: 7}
 
