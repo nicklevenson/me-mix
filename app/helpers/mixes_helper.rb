9fd3080c04afb5e9207b39c54a378b93e74a067a
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-12 18:25:58 -0800

    before deletions

--- app/helpers/mixes_helper.rb
+++ app/helpers/mixes_helper.rb
@@ -2,12 +2,14 @@ module MixesHelper
   def create_media(mix)
     data = eval(params[:media])
     if data[:data_type] == "music"
-      music = Music.find_or_create_by(data)
+      music = Music.find_or_create_by(url: data[:url])
+      music.update(data)
       mix.musics << music unless mix.musics.include?(music)
       
-      byebug
     elsif data[:data_type] == "poem"
-      
+      poem = Poem.find_or_create_by(url: data[:url])
+      poem.update(data)
+      mix.poems << poem unless mix.poems.include?(poem)
     elsif data[:data_type] == "movie"
       
     elsif data[:data_type] == "book"
